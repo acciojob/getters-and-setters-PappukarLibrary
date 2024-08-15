29be5f3f41f6c45a8a7256598c3f9c9b96f1f779
PappukarLibrary <158065740+PappukarLibrary@users.noreply.github.com>
2024-08-15 11:40:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 //complete this code
-class Person {}
+class Person {
+	const person = new Person("John",25);
+	console.log(person.name);
+	person.age=25;
+	console.log(person.age);
+}
 
-class Student extends Person {}
 
-class Teacher extends Person {}
+class Student extends Person {
+	const student = new Student("pappu",22);
+
+	const study (){
+		console.log("Pappu is Studying");
+		
+	}
+return	student.study();
+}
+
+class Teacher extends Person {
+	const teacher = new Teacher("Bob", 40);
+	
+	const teach (){
+		console.log("Bob is teaching");
+		
+	}
+return	teacher.teach();
+	
+}
 
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
+
